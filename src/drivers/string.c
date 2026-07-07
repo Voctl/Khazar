1afe8c0dc3d7c47b61d50bd6cbd9d460873a2ef1
Voctl <denis.3131a@gmail.com>
2026-07-08 03:31:23 +0400

    I made a memcpy func for kheap

--- src/drivers/string.c
+++ src/drivers/string.c
@@ -1,14 +1,23 @@
-#include "../include/typint.h"
-#include "../include/vga.h"
 #include "../include/string.h"
 #include "../include/memory/pmm.h"
+#include "../include/typint.h"
+#include "../include/vga.h"
 
-void *memset(void *ptr, int value, size_t num)
-{
-    U8 *p = ptr;
+void *memset(void *ptr, int value, size_t num) {
+  U8 *p = ptr;
 
-    while (num--)
-        *p++ = (U8)value;
+  while (num--)
+    *p++ = (U8)value;
 
-    return ptr;
+  return ptr;
+}
+
+void *memcpy(void *dest, const void *src, size_t num) {
+  U8 *d = dest;
+  const U8 *s = src;
+
+  while (num--)
+    *d++ = *s++;
+
+  return dest;
 }
--- src/include/string.h
+++ src/include/string.h
@@ -2,3 +2,4 @@
 #include "typint.h"
 
 void *memset(void *ptr, int value, size_t num);
+void *memcpy(void *dest, const void *src, size_t num);
