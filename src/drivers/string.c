36a494f036155ec379497e18817a05e398d5ada9
Voctl <denis.3131a@gmail.com>
2026-07-17 03:24:01 +0400

    string

--- src/drivers/string.c
+++ src/drivers/string.c
@@ -30,3 +30,11 @@ char *strcpy(char *dest, const char *src) {
     *dest = '\0'; // NULL terminator
     return ptr;
 }
+
+I32 strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
