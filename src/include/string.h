4aef7a26309f657846bd64de7b53e6d99477ca94
Voctl <denis.3131a@gmail.com>
2026-07-17 03:23:39 +0400

    string

--- src/include/string.h
+++ src/include/string.h
@@ -3,3 +3,5 @@
 
 void *memset(void *ptr, int value, size_t num);
 void *memcpy(void *dest, const void *src, size_t num);
+char *strcpy(char *dest, const char *src);
+I32 strcmp(const char *s1, const char *s2);
