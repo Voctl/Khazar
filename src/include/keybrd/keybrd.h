03ea30483daee37c2734297a1ec0378e8f1a6ca1
Voctl <denis.3131a@gmail.com>
2026-07-17 03:45:33 +0400

    arrow keys

--- src/include/keybrd/keybrd.h
+++ src/include/keybrd/keybrd.h
@@ -4,7 +4,13 @@
 #include "../isr.h"
 #include "../typint.h"
 
-void keyboard_handler(registers_t *regs) ;
+#define ARROW_KEY_UP    ((char)0x01)
+#define ARROW_KEY_DOWN  ((char)0x02)
+#define ARROW_KEY_LEFT  ((char)0x03)
+#define ARROW_KEY_RIGHT ((char)0x04)
+
+void keyboard_handler(registers_t *regs);
 void keyboard_init(void);
+BOOL keyboard_poll_char(char *out);
 
 #endif
