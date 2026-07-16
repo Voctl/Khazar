62b861ffa2ea7c9ec0eb70961d6e26613308519e
Voctl <denis.3131a@gmail.com>
2026-07-17 03:45:58 +0400

    arrow keys

--- src/drivers/keybrd/keybrd.c
+++ src/drivers/keybrd/keybrd.c
@@ -6,8 +6,8 @@
 #include "../../include/io.h"
 
 #define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_INT 33 // IRQ1 + 32 (IDT offset)
-
+#define KEYBOARD_INT 33
+#define KB_BUF_SIZE 128
 
 static const char base_map[128] = {
 	[0x02] = '1', [0x03] = '2', [0x04] = '3', [0x05] = '4',
@@ -24,7 +24,7 @@ static const char base_map[128] = {
 	[0x2b] = '\\',
 	[0x2c] = 'z', [0x2d] = 'x', [0x2e] = 'c', [0x2f] = 'v',
 	[0x30] = 'b', [0x31] = 'n', [0x32] = 'm', [0x33] = ',',
-	[0x34] = '.', [0x35] = '/', [0x39] = ' ', [0x01] = '\x1b',
+	[0x34] = '.', [0x35] = '/', [0x39] = ' ', [0x01] = 0,
 };
 
 static const char shift_map[128] = {
@@ -42,10 +42,28 @@ static const char shift_map[128] = {
 	[0x2b] = '|',
 	[0x2c] = 'Z', [0x2d] = 'X', [0x2e] = 'C', [0x2f] = 'V',
 	[0x30] = 'B', [0x31] = 'N', [0x32] = 'M', [0x33] = '<',
-	[0x34] = '>', [0x35] = '?', [0x39] = ' ', [0x01] = '\x1b',
+	[0x34] = '>', [0x35] = '?', [0x39] = ' ', [0x01] = 0,
 };
 
 static U8 shift_on = 0;
+static char kb_buffer[KB_BUF_SIZE];
+static volatile U8 kb_head = 0;
+static volatile U8 kb_tail = 0;
+
+static void kb_push(char c) {
+	U8 next = (U8)((kb_head + 1) % KB_BUF_SIZE);
+	if (next != kb_tail) {
+		kb_buffer[kb_head] = c;
+		kb_head = next;
+	}
+}
+
+BOOL keyboard_poll_char(char *out) {
+	if (kb_head == kb_tail) return FALSE;
+	*out = kb_buffer[kb_tail];
+	kb_tail = (U8)((kb_tail + 1) % KB_BUF_SIZE);
+	return TRUE;
+}
 
 void keyboard_handler(registers_t *regs) {
 	(void)regs;
@@ -56,15 +74,24 @@ void keyboard_handler(registers_t *regs) {
 	} else if (sc == 0xAA || sc == 0xB6) {
 		shift_on = 0;
 	} else if (!(sc & 0x80)) {
-		char c = shift_on ? shift_map[sc] : base_map[sc];
-		if (c) kbd_putchar(c);
+		char c = 0;
+		switch (sc) {
+			case 0x48: c = ARROW_KEY_UP;    break;
+			case 0x50: c = ARROW_KEY_DOWN;  break;
+			case 0x4B: c = ARROW_KEY_LEFT;  break;
+			case 0x4D: c = ARROW_KEY_RIGHT; break;
+			default:
+				c = shift_on ? shift_map[sc] : base_map[sc];
+				break;
+		}
+		if (c) kb_push(c);
 	}
 }
 
 void keyboard_init(void) {
 	register_interrupt_handler(KEYBOARD_INT, keyboard_handler);
 
-	U8 mask = byte_i(0x21);   // Master PIC data/mask port
-	mask &= ~(1 << 1);       // remove IRQ1 mask
+	U8 mask = byte_i(0x21);
+	mask &= ~(1 << 1);
 	byte_o(0x21, mask);
 }
