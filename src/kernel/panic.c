e69019b7b6425666aa302cdf9eaa829b46fba6e6
Voctl <denis.3131a@gmail.com>
2026-07-18 03:40:45 +0400

    panic: add STR8_C casts to fix -Wpointer-sign warnings

--- src/kernel/panic.c
+++ src/kernel/panic.c
@@ -3,9 +3,9 @@
 
 void kernel_panic(const char* msg) {
     asm volatile("cli");
-    putstr_color("\n[ KERNEL PANIC ] ", COLOR_LIGHT_RED);
-    putstr(msg);
-    putstr("\n");
+    putstr_color((STR8_C)"\n[ KERNEL PANIC ] ", COLOR_LIGHT_RED);
+    putstr((STR8_C)msg);
+    putstr((STR8_C)"\n");
     while (1) asm volatile("hlt");   //stop
 }
 
