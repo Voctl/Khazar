8acbaad10aa83e27f1a85924da621127c4ba3e1a
Voctl <denis.3131a@gmail.com>
2026-08-26 19:07:29 +0400

    new funcs for libk

--- src/libk/string.h
+++ src/libk/string.h
@@ -5,3 +5,7 @@ void *memset(void *ptr, int value, size_t num);
 void *memcpy(void *dest, const void *src, size_t num);
 char *strcpy(char *dest, const char *src);
 I32 strcmp(const char *s1, const char *s2);
+// tdy
+void *memmove(void *dest, const void *src, size_t num);
+size_t strlen(const char *s);
+I32 strncmp(const char *s1, const char *s2, size_t n);
