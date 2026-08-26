bc56be6f449bb69b16cd72cd1ef13ef1f08a65ea
Voctl <denis.3131a@gmail.com>
2026-08-26 16:43:46 +0400

    TODO

--- src/libk/string.c
+++ src/libk/string.c
@@ -5,17 +5,14 @@
 
 void *memset(void *ptr, int value, size_t num) {
   U8 *p = ptr;
-
   while (num--)
     *p++ = (U8)value;
-
   return ptr;
 }
 
 void *memcpy(void *dest, const void *src, size_t num) {
   U8 *d = dest;
   const U8 *s = src;
-
   while (num--)
     *d++ = *s++;
 
@@ -38,3 +35,8 @@ I32 strcmp(const char *s1, const char *s2) {
     }
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+
+/*There is only 4 functions in here right now :
+ * (memset, memcpy, strcpy, strcmp).*/
+
+// TODO : memmove, strlen, strncmp, strchr, strstr, strtol/atoi, qsort
