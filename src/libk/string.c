33b10afee340a618eed99d477ec82df37c7920d9
Voctl <denis.3131a@gmail.com>
2026-08-26 17:39:44 +0400

    [added] memmove function

--- src/libk/string.c
+++ src/libk/string.c
@@ -3,6 +3,8 @@
 #include "types.h"
 #include "vga.h"
 
+
+/* Fills memory block with a byte value */
 void *memset(void *ptr, int value, size_t num) {
   U8 *p = ptr;
   while (num--)
@@ -10,6 +12,7 @@ void *memset(void *ptr, int value, size_t num) {
   return ptr;
 }
 
+/* Copies non-overlapping memory area */
 void *memcpy(void *dest, const void *src, size_t num) {
   U8 *d = dest;
   const U8 *s = src;
@@ -19,6 +22,7 @@ void *memcpy(void *dest, const void *src, size_t num) {
   return dest;
 }
 
+/* Copies null-terminated string */
 char *strcpy(char *dest, const char *src) {
     char *ptr = dest;
     while (*src != '\0') {
@@ -28,6 +32,7 @@ char *strcpy(char *dest, const char *src) {
     return ptr;
 }
 
+/* Compares two strings byte-by-byte */
 I32 strcmp(const char *s1, const char *s2) {
     while (*s1 && (*s1 == *s2)) {
         s1++;
@@ -35,6 +40,19 @@ I32 strcmp(const char *s1, const char *s2) {
     }
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+
+/*Copy memory area*/
+void *memmove(void dest, const void src, size_t num){
+  U8 *d = dest;
+  const U8 *s = src;
+  if (d < s) return memcpy(dest, src, num);
+  while (num--){
+      d[num] = s[num];
+  }
+  return dest
+}
+
+
 
 /*There is only 4 functions in here right now :
  * (memset, memcpy, strcpy, strcmp).*/
