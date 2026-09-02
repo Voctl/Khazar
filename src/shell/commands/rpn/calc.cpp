6168c36ffaa82a7fe2d6727c5fb2163c4ed94f72
Voctl <denis.3131a@gmail.com>
2026-09-02 05:52:35 +0400

    added cpp bloat shi

---
+++ src/shell/commands/rpn/calc.cpp
@@ -0,0 +1,15 @@
+/*Its the simple cpp project
+ *for RPN calculator
+ *of Khazar hobby OS project */
+
+extern "C" {
+#include "types.h"
+#include "vga.h"
+#include "string.h"
+#include "rpn/calc.h"
+};
+
+#define STACK_MAX 32
+
+class Stack{
+};
