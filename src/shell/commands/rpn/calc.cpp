c03cbb28135e45f2cb9e07c6b8430b5fff943bfa
Voctl <denis.3131a@gmail.com>
2026-09-02 15:32:04 +0400

    [add] stack etc.

--- src/shell/commands/rpn/calc.cpp
+++ src/shell/commands/rpn/calc.cpp
@@ -12,4 +12,12 @@ extern "C" {
 #define STACK_MAX 32
 
 class Stack{
+public:
+    Stack() : top(0) {}
+    void push(I64 value);
+    I64 pop();
+    bool empty() const;
+private:
+    I64 data[STACK_MAX];
+    int top;
 };
