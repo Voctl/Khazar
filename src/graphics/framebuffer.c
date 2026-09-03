004bb894a87763152987d186308fa65a5f9ebfbf
Voctl <denis.3131a@gmail.com>
2026-09-03 14:39:44 +0400

    what did i fixed

--- src/graphics/framebuffer.c
+++ src/graphics/framebuffer.c
@@ -5,9 +5,14 @@
 U0 testFrameb(multiboot2_tag_framebuffer_t *fb){
     putstr((STR8_C)"FrameBuffer detected !");
 
-    putstr((STR8_C)"Addr:  "); putdec((U64)fb->framebuffer_addr); putstr((STR8_C)"\n");
-    putstr((STR8_C)"Width:  "); putdec(fb->framebuffer_width); putstr((STR8_C)"\n");
-    putstr((STR8_C)"Height:  "); putdec(fb->framebuffer_height); putstr((STR8_C)"\n");
-    putstr((STR8_C)"Pitch:  "); putdec(fb->framebuffer_pitch); putstr((STR8_C)"\n");
-    putstr((STR8_C)"BPP:  "); putdec(fb->framebuffer_bpp); putstr((STR8_C)"\n");
+    putstr((STR8_C)"Addr:  ");
+    putdec((U64)fb->framebuffer_addr); putstr((STR8_C)"\n");
+    putstr((STR8_C)"Width:  ");
+    putdec(fb->framebuffer_width); putstr((STR8_C)"\n");
+    putstr((STR8_C)"Height:  ");
+    putdec(fb->framebuffer_height); putstr((STR8_C)"\n");
+    putstr((STR8_C)"Pitch:  ");
+    putdec(fb->framebuffer_pitch); putstr((STR8_C)"\n");
+    putstr((STR8_C)"BPP:  ");
+    putdec(fb->framebuffer_bpp); putstr((STR8_C)"\n");
 }
